afd27e93e56d39664e3695a916d45d90bbfe0905
Marina <MarinaPetspb@gmail.com>
2018-04-22 20:16:27 +0300

    Send_code

--- Test_promokod.py
+++ Test_promokod.py
@@ -6,11 +6,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-def check_elem(text):
+def check_xpath(text):
     WebDriverWait(driver, 120).until(
         EC.element_to_be_clickable((By.XPATH, text))
     )
 
+def check_name(text):
+    WebDriverWait(driver, 120).until(
+        EC.element_to_be_clickable((By.NAME, text))
+    )
 phone=1111111111
 button_send='//button'
 driver = webdriver.Chrome('C:/Users/User/Documents/DocDoc/Autotesting/chromedriver.exe')
@@ -21,9 +25,11 @@ driver.find_element_by_link_text('Войти').click()
 driver.find_element_by_xpath('//input').send_keys(phone)
 driver.find_element_by_xpath(button_send).click()
 # вводим код авторизации
-check_elem('//input')
-elem=driver.find_element_by_class_name('mask-phone')
+check_name("code")
+elem=driver.find_element_by_name("code")
 elem.send_keys("5555")
 elem.send_keys(Keys.RETURN)
-check_elem('//button[@value="Подтвердить"]')
+#отправляем код авторизации
+check_xpath('//button[@value="Подтвердить"]')
 driver.find_element_by_xpath('//button[@value="Подтвердить"]').click()
+driver.close()
